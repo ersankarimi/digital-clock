b9755d142cd4a6e878eab679735ec25ded85a433
Ersan Karimi <ersanb19@gmail.com>
2021-10-14 20:50:12 +0800

    "buat item baru untuk ditaruh di local storage yaitu setTodoAlready(), setTodoOpened(), dan setTodoList()"

--- js/storage.js
+++ js/storage.js
@@ -3,13 +3,12 @@ const cacheName = "NAME";
 const cacheWallpaper = "WALLPAPER";
 const cacheSecond = "SECOND";
 const cacheQuotes = "QUOTES";
+const cacheTodoAlready = "TODO_ALREADY";
+const cacheTodoOpened = "TODO_OPENED";
+const cacheItemTodoList = "TODO_LIST_ITEM";
 
 // punya clock
 function checkStatusClockFormat(el) {
-    if (el == "true") {
-        localStorage.setItem(cacheKey, el);
-    };
-
     localStorage.setItem(cacheKey, el);
 };
 
@@ -51,4 +50,31 @@ function setQuotes(value) {
 
 function getQuotes() {
     return localStorage.getItem(cacheQuotes);
-}
+};
+
+// punya button new todo
+function setTodoAlready(value) {
+    localStorage.setItem(cacheTodoAlready, value);
+};
+
+function getTodoAlready() {
+    return localStorage.getItem(cacheTodoAlready);
+};
+
+// punya title todo pojok kanan bawah
+function setTodoOpened(value) {
+    localStorage.setItem(cacheTodoOpened, value);
+};
+
+function getTodoOpened() {
+    return localStorage.getItem(cacheTodoOpened);
+};
+
+// punya list item todo list
+function setItemTodoList(value) {
+    localStorage.setItem(cacheItemTodoList, value);
+};
+
+function getItemTodoList() {
+    return localStorage.getItem(cacheItemTodoList);
+};
