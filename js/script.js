c1133c1b982dc9b23255557bdd1e4ae57faf52f1
Ersan Karimi <ersanb19@gmail.com>
2021-10-14 20:52:31 +0800

    "buat function baru showTodoList(), checkShowTodoList(), hideButtonNewTodoList(), switchTodoList()"

--- js/script.js
+++ js/script.js
@@ -26,10 +26,22 @@ moreButton.addEventListener("click", function () {
 });
 
 // get checkbox for clock format element variabel
-let clockFormatToggle = container.querySelector(".onoffswitch-checkbox-clockformat");
+const clockFormatToggle = container.querySelector(".onoffswitch-checkbox-clockformat");
 
 // get checkbox for show second element variabel
-let showSecondToggle = container.querySelector(".onoffswitch-checkbox-showsecond");
+const showSecondToggle = container.querySelector(".onoffswitch-checkbox-showsecond");
+
+// variabel element dari title todo pojok kanan bawah
+const todoTitleRight = container.querySelector("#todo-opened");
+
+// variabel to get element button new todo
+const buttonNewTodo = container.querySelector(".btn-new-todo");
+
+// variabel element dari todo list wrapper (container nya)
+const todoListWrapper = container.querySelector(".todo-list-wrapper");
+
+// variabel untuk element input new todo
+const newTodoInput = container.querySelector("#new-todo-input");
 
 // list text to display on screen varibel
 const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -58,7 +70,7 @@ const sayingTwo = {
     "thursday": {
         "morning": [`Good morning, ${getNameFromLocalStorage()}.`, `You can do it.`, `Be nice.`], 
         "afternoon": [`Good afternoon, ${getNameFromLocalStorage()}.`, `Grab your coffee.`, `Keep it up!`],
-        "evening": [`Good evening, ${getNameFromLocalStorage()}.`, `Sleep well ${getNameFromLocalStorage()}.`, `Take a rest ${getNameFromLocalStorage()}!`]
+        "evening": [`Good evening, ${getNameFromLocalStorage()}.`, `Sleep well, ${getNameFromLocalStorage()}.`, `Take a rest, ${getNameFromLocalStorage()}!`]
     },
     "friday": {
         "morning": [`Good morning, ${getNameFromLocalStorage()}.`, `Prayer changes everything.`, `Be thankful.`],
@@ -74,6 +86,7 @@ const sayingTwo = {
 
 const quotes = ["Someday is not a day of the week.", "Your time is limited, so don't waste it living someone else's life.", "If you are not taking care of your customer, your competitor will.", "Beware of monotony; it's the mother of all the deadly sins.", "Nothing is really work unless you would rather be doing something else.", "Be patient with yourself. Self-growth is tender, it's holy ground. There's no greater investment.", "If you never try , you'll never know.", "Without hustle, talent will only carry you so far.", "Working hard for something we don't care about is called stressed, working hard for something we love is called passion.", "I'd rather regret the things I've done than regret the things I haven't done.", "Monday morning you sure look fine.", "The biggest thrill wasn't in winning on Sunday but in meeting the payroll on Monday.", "Time is what we want most and what we use worst.", "Only you can change your life. Nobody else can do it for you.", "Everyone thinks of changing the world, but no one thinks of changing himself.", "Don’t stop when you are tired. Stop when you are done.", "Happiness is not something ready made. It comes from your own actions.", "You can either experience the pain of discipline or the pain of regret. The choice is yours.", "Words can inspire, thoughts can provoke, but only action truly brings you closer to your dreams.", "Do something today that your future self will thank you for.", "Success is not final. Failure is not fatal. It is the courage to continue that counts.", "Be strong enough to let go and wise enough to wait for what you deserve.", "Live as if you were to die tomorrow. Learn as if you were to live forever.", "When the pain of an obstacle is too great, challenge yourself to be stronger.", "The difference between who you are and who you want to be is what you do.", "Success is going from failure to failure without losing your enthusiasm.", "Stop being afraid of what can go wrong and start being positive about what can go right.", "It is better to fail in originality than to succeed in imitation.", "Your mind is powerful. When you fill it with positive thoughts your whole world will change.", "You were born to win, but to be a winner, you must plan to win, prepare to win, and expect to win.", "A negative mind will never give you a positive life.", "Time is what we want most and what we use worst.", "Better three hours too soon than a minute too late.", "Everyone thinks of changing the world, but no one thinks of changing himself.", "The simple act of paying attention can take you a long way.", "Happiness is not something ready made. It comes from your own actions.", "You can either experience the pain of discipline or the pain of regret. The choice is yours.", "Do something today that your future self will thank you for.", "Be strong enough to let go and wise enough to wait for what you deserve.", "When the pain of an obstacle is too great, challenge yourself to be stronger.", "Be the change that you wish to see in the world.", "I never dreamed about success. I worked for it.", "Don’t be afraid to give up the good to go for the great.", "The difference between who you are and who you want to be is what you do.", "Never stop learning because life never stops teaching.", "Stop dreaming, and start doing.", "If you never try , you'll never know.", "The more you learn, the more you earn.", "Experience is a hard teacher because she gives the test first, the lesson afterwards."]
 
+let todoListItem = [];
 
 
 // function rotate quote
@@ -107,7 +120,6 @@ function sayingdisplay12(hrs) {
 
 // function show quotes
 function showQuotes() {
-    console.log(getQuotes());
     return (getQuotes() == null) ? setQuotes(quotes[Math.floor(Math.random() * quotes.length)]) : getQuotes();
 };
 
@@ -185,8 +197,7 @@ function switchClockFormat() {
             displaySaying.innerText = sayingdisplay12(new Date)
         };
 
-        let el = clockFormatToggle.checked.toString()
-        checkStatusClockFormat(el);
+        checkStatusClockFormat(clockFormatToggle.checked.toString());
     });
 };
 
@@ -234,3 +245,58 @@ function giveCheckedAttribute(param1, param2) {
         container.querySelector("#myonoffswitch-showsecond").checked = false;
     };
 };
+
+// event click pada todo title right
+function SwitchShowTodoList(value){
+    todoTitleRight.addEventListener("click", () => {
+        if (value == "false") {
+            setTodoOpened("true")
+            todoListWrapper.style.display = "flex";
+        } else {
+            setTodoOpened("false");
+            todoListWrapper.style.display = "none";
+
+            if (todoListItem.length  == 0) {
+                buttonNewTodo.style.display = "inline-block";
+                newTodoInput.style.display = "none";
+            } else {
+                buttonNewTodo.style.display = "none";
+                newTodoInput.style.display = "block";
+            }
+        }
+    });
+}
+
+// function untuk menampilkan atau tidak pada tombol new todo
+// berdasarkan value dari local storage TODO_OPENED nya true atau false.
+function showTodoList(value) {
+    value == "true" ? todoListWrapper.style.display = "flex" : todoListWrapper.style.display = "none";
+};
+
+// check ketika dokumen dimuat apakah akan menampilkan todo list
+// atau tidak menmapilkan todo list
+function checkShowTodoList() {
+    if (getTodoOpened() == null || getTodoOpened() == "false") {
+        todoListWrapper.style.display = "none";
+        setTodoOpened("false");
+    } else {
+        todoListWrapper.style.display = "flex";
+        setTodoOpened("true");
+    };
+};
+
+// function hide todo button when click and move cursor to etxt input
+function hideButtonNewTodo() {
+    buttonNewTodo.addEventListener("click", () => {
+        buttonNewTodo.style.display = "none";
+        newTodoInput.style.display = "block";
+        newTodoInput.focus();
+    });
+};
+
+// function untuk set text input new todo
+// jika balum membuat todolist sama sekali (0 panjang array todolist (pada variabel todoListItem))
+// maka akan hide dia (kecuali tombol new todo di clik)
+// function hideTextInputTodoList() {
+//     todoListItem.length === 0 ? newTodoInput.style.display = "none" : newTodoInput.style.display = "block";
+// };
